5dc0d5e12d7aa0d70d31d6a63de765a63016ebc8
chinmayiii <chinmayibellippadi@gmail.com>
2026-03-21 17:49:56 +0530

    initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * Hello world!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Maven to Gradle migration successful." );
+    }
+}
